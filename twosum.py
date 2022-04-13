3ad3765989c7e45cc2636a3cb24f91e9339471fd
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-13 20:39:18 +0000

    update

--- twosum.py
+++ twosum.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/two-sum/submissions/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+            diffs = {}
+            for i in range(len(nums)):
+                if nums[i] in diffs:
+                    res=[diffs[nums[i]],i]
+                diff = target - nums[i]
+                diffs[diff] = i
+            return res
