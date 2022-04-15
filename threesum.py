8c2d0266d47b724e9fb8d40946a2e27d8ea657cf
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-15 16:50:48 +0000

    update

--- threesum.py
+++ threesum.py
@@ -16,8 +16,6 @@ def threeSum(nums):
         hashmap = {}
         for k in range(i + 1, len(nums)):
             diff = target - nums[k]
-            if diff in hashmap:
-                continue
             if nums[k] in hashmap:
                 j = hashmap[nums[k]]
                 res.append((nums[i],nums[j],nums[k]))
@@ -25,5 +23,32 @@ def threeSum(nums):
             print(hashmap)
     return res
 
+#??????????????????
 # not good  as can not reduce  inside loop duplicate
 print(threeSum([0,0,0,0]))
+
+
+# solution
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        
+        for i, a in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threesum = a + nums[l] + nums[r]
+                if threesum > 0:
+                    r -=1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    res.append([a,nums[l],nums[r]])
+                    l += 1
+					# only need to update the left pointer to avoid duplicate
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+        return res
