02a1d25c1b844c1aacabf48e3f8d956fa051dad4
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-15 14:09:07 +0000

    update

--- threesum.py
+++ threesum.py
@@ -1,21 +1,29 @@
 # https://leetcode.com/problems/3sum/
 # sort the array to get rid of the dupulicate
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        A = {}
-        for i in range(len(nums)):
-            if nums[i] in A:
+
+def threeSum(nums):
+    nums.sort()
+    res = []
+    A = {}
+    for i in range(len(nums)):
+        print(i)
+        if nums[i] in A:
+            continue
+        else:
+            A[nums[i]] = i
+        print(A)
+        target = 0 - nums[i]
+        hashmap = {}
+        for k in range(i + 1, len(nums)):
+            diff = target - nums[k]
+            if diff in hashmap:
                 continue
-            else:
-                A[nums[i]] = i
-            target = 0 - nums[i]
-            hashmap = {}
-            for k in range(i + 1, len(nums)):
-                diff = target - nums[k]
-                if nums[k] in hashmap:
-                    j = hashmap[nums[k]]
-                    res.append((nums[i],nums[j],nums[k]))
-                hashmap[diff] = k
-        return res
+            if nums[k] in hashmap:
+                j = hashmap[nums[k]]
+                res.append((nums[i],nums[j],nums[k]))
+            hashmap[diff] = k
+            print(hashmap)
+    return res
+
+# not good  as can not reduce  inside loop duplicate
+print(threeSum([0,0,0,0]))
