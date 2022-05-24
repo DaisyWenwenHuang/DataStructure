bfb009aaa7c0442aaf2020a7e529705d60f98fde
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-24 19:22:32 +0000

    done

--- maxsubarray.py
+++ maxsubarray.py
@@ -1,2 +1,14 @@
 # https://leetcode.com/problems/maximum-subarray/
 # array
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxsum = nums[0]
+        cursum = 0
+        
+        for n in nums:
+            
+            if cursum < 0:
+                cursum = 0
+            cursum += n
+            maxsum = max(cursum,maxsum)
+        return maxsum
