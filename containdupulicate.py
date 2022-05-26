64d960a1abf5772e4f248f967b631d16103e522f
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-26 21:43:19 +0000

    update

--- containdupulicate.py
+++ containdupulicate.py
@@ -1,2 +1,10 @@
 # https://leetcode.com/problems/contains-duplicate/
 # array
+# using set
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numset = set(nums)
+        if len(numset) == len(nums):
+            return False
+        else:
+            return True
