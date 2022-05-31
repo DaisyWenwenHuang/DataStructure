9d0c552515d084ed730f415730a96571267c5ec5
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-31 17:17:11 +0000

    update

--- rotatearray.py
+++ rotatearray.py
@@ -1,2 +1,20 @@
 # https://leetcode.com/problems/rotate-array/
 # array
+# solution 1
+# 
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        # edge case:
+        if k == 0 or n == 1 or k == n:
+            return 
+        # K could be bigger than n
+        k = k % n
+        
+        for i in range(k):
+            ele = nums.pop()
+            nums.insert(0,ele)
+            
