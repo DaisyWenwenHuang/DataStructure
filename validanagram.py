0fc9a883f7d1e84da9d79436a2ea82680900cf5b
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-06-15 22:21:18 +0000

    update

--- validanagram.py
+++ validanagram.py
@@ -2,3 +2,15 @@
 # hashtable
 # anagram: a word of phrase formed by rearranging the letters of a different
 # word or phrase, typically using all the original letters exactly once
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hashtable = {}
+        for char in s:
+            if char in hashtable:
+                hashtable[char] += 1
+            else:
+                hashtable.get(char,0)
+        for ele in t:
+            
